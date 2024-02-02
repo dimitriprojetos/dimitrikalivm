be3c2583561fc43d0440c71ffc5ac0d95029a8a3
Dimitri Martins <158604906+dimitriprojetos@users.noreply.github.com>
2024-02-02 18:18:40 -0300

    ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -5,7 +5,7 @@ stty susp undef
 
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-echo "Kali - Acesso Remoto (VM)"
+echo "Kali Linux - Acesso Remoto (VM)"
 echo "By Dimitri"
 echo "Baixando Ngrok..."
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
